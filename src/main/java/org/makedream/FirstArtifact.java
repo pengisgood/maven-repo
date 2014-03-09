e3e18f0545af14c564a6a629df74c4d1e1df2f65
Max Peng <pengisgood@gmail.com>
2014-03-09 13:40:48 +0800

    minor change

--- src/main/java/org/makedream/FirstArtifact.java
+++ src/main/java/org/makedream/FirstArtifact.java
@@ -2,6 +2,6 @@ package org.makedream;
 
 public class FirstArtifact {
     public String say(String content) {
-        return String.format("你叫我说，我就说：%s", content);
+        return String.format("你叫我说，我就不说：%s", content);
     }
 }
